067a3ea7b744dbbf387e3447bd82a7377cd7b509
dnxroshan <dilnavasroshan@gmail.com>
2020-11-25 23:28:13 +0530

    Registered models Category and Product on the admin site

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CtegoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'slug', 'price', 
+        'available', 'created', 'updated'
+    ]
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['available', 'price']
+    prepopulated_fields = {'slug': ('name', )}

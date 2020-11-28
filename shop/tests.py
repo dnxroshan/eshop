c8d1388a148313cd7720b063d49ffa92ee0c2bf5
dnxroshan <dilnavasroshan@gmail.com>
2020-11-28 17:15:06 +0530

    removed test test!!

--- shop/tests.py
+++ shop/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .models import Category, Product
 
+'''
 class TrivialTest(TestCase):
     def test_trivial(self):
         assert True == False, 'True-False test failed'
+'''
